0f9f5c5787c9063670f4c7ea82ca5191c5ad7608
Darren <nardol.darren@gmail.com>
2024-03-21 22:58:45 +0100

    create card

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrl: './card.component.css',
 })
 export class CardComponent {
-
+  @Input() alt = "Ceci est la description de l'image";
+  @Input() src = '/assets/img/Logo.png';
+  @Input() title = 'Titre par défault';
+  @Input() description = 'Ceci est la description du produit';
+  @Input() prix = 'X€';
 }
